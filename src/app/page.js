a774b3d5bd111a2910355c0f5de5620b81c4ac48
mrphoenixuz <karimovmoff@gmail.com>
2025-01-09 19:44:25 +0500

    haa

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Categories from '@/components/Categories'
 import Featured from '@/components/Featured'
 import FlashSales from '@/components/FlashSales'
 import Hero from '@/components/Hero'
+import Services from '@/components/Services'
 import Head from 'next/head'
 import React from 'react'
 
@@ -12,6 +13,7 @@ const HomePage = () => {
       <FlashSales />
       <Categories />
       <Featured />
+      <Services />
     </div>
   )
 }
